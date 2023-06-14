bebb676f5d4f16bc69569a8576defa6672284328
Alona Panchenko <97801671+Enotishche@users.noreply.github.com>
2023-06-14 03:21:12 +0300

    Update StartPage.jsx

--- src/pages/Start/StartPage.jsx
+++ src/pages/Start/StartPage.jsx
@@ -1,15 +1,15 @@
-//import AuthSection from 'components/Start/AuthSection/AuthSection';
-//import Description from 'components/Start/Description/Description';
+import AuthSection from 'components/Start/AuthSection/AuthSection';
+import Description from 'components/Start/Description/Description';
 import ReviewsSlider from 'components/Start/ReviewsSlider/ReviewsSlider';
 import Footer from 'components/Start/Footer/Footer';
-//import { ThemeToggler } from 'components/MainLayout/Header/ThemeToggler/ThemeToggler';
+import { ThemeToggler } from 'components/MainLayout/Header/ThemeToggler/ThemeToggler';
 
 const StartPage = () => {
   return (
     <>
-      {/* <ThemeToggler />
+      <ThemeToggler />
       <AuthSection />
-      <Description /> */}
+      <Description />
       <ReviewsSlider />
       <Footer />
     </>
